4ef1a0bbeea80bb10d8f50b61c3028d8ebc0642f
Osman KURT <osman.kurt@msn.com>
2020-05-31 14:14:50 +0300

    login edit

--- RabbitMQService.cs
+++ RabbitMQService.cs
@@ -5,12 +5,16 @@ namespace RabbitMQExample
     public class RabbitMQService
     {
         private readonly string _hostName = "localhost";
+        private readonly string _userName = "admin";
+        private readonly string _password = "123456";
 
         public IConnection GetRabbitMQConnection()
         {
             ConnectionFactory connectionFactory = new ConnectionFactory()
             {
-                HostName = _hostName
+                HostName = _hostName,
+                UserName = _userName,
+                Password = _password
             };
 
             return connectionFactory.CreateConnection();
